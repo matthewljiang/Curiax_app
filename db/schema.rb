3297685bc2a84a7725be60a412b776d709c8815a
Matthew Jiang <matthew.jiang@troopswap.com>
2013-08-14 09:55:51 -0400

    Fix homepage

--- db/schema.rb
+++ db/schema.rb
@@ -58,11 +58,6 @@ ActiveRecord::Schema.define(version: 20130803173341) do
   add_index "companies", ["email"], name: "index_companies_on_email", unique: true
   add_index "companies", ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
 
-  create_table "homes", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "pages", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
